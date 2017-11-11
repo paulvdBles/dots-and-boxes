eb8d10530d8c0f16f5ad848b4e2c0e06f4f1edef
Paul <paulvdb08@hotmail.com>
2017-11-11 21:49:15 +0100

    Still working on draw board algorithm. Dots placing seem to work

--- src/Controller/BoardController.java
+++ src/Controller/BoardController.java
@@ -3,8 +3,8 @@ package Controller;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
@@ -23,23 +23,69 @@ public class BoardController {
     @FXML
     private Pane boardPane;
 
+    private int dots = 0;
+    private int lines = 0;
+
     void setPrimaryScene(Scene primaryScene) {
         this.primaryScene = primaryScene;
     }
 
     public void drawBoard(int columns, int rows) {
+        float positionX = 5;
+        float positionY = 5;
+
         // draw the dots on the board
-        for (int currentRow = 0; currentRow < rows; currentRow++) {
-            StringBuilder str = new StringBuilder();
-            for (int currentColumn = 0; currentColumn < columns; currentColumn++) {
-                str.append("[] ");
+        for (int currentRow = 0; currentRow <= rows; currentRow++) {
+            for (int currentColumn = 0; currentColumn <= columns; currentColumn++) {
+                    createDotAt(positionX, positionY);
+                    positionX += 88;
             }
-            System.out.println(str);
+            positionX -= (88 * (columns + 1));
+            positionY += 88;
         }
-        boardPane.getChildren().add(new Rectangle());
+        System.out.println("Drawed dots: " + dots);
+        System.out.println("Drawed lines: " + lines);
 
         // draw the lines on the board
 
+    }
+
+    private void createInnerCubeAt(float positionX, float positionY) {
+        // the inner cube you know
+    }
+
+    private void createDotAt(float positionX, float positionY) {
+        Rectangle dot = new Rectangle();
+
+        dot.setHeight(15);
+        dot.setWidth(15);
+        dot.setLayoutX(positionX); // so it went 88 places to the right
+        dot.setLayoutY(positionY);
+        dot.setFill(Color.valueOf("#363b40"));
+
+        boardPane.getChildren().add(dot);
+
+        dots += 1;
+    }
+
+    private void createLineAt(float positionX, float positionY, boolean horizontal) {
+        Rectangle line = new Rectangle();
+
+        if (horizontal) {
+            line.setHeight(15);
+            line.setWidth(75);
+        } else {
+            line.setHeight(75);
+            line.setWidth(15);
+        }
+
+        line.setLayoutX(positionX + 14); // so it went 88 places to the right
+        line.setLayoutY(positionY);
+        line.setFill(Color.valueOf("#d0e1f2"));
+
+        boardPane.getChildren().add(line);
+
+        lines += 2;
     }
 
     public void lineClicked() {
